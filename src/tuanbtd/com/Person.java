9562a901537e6f952d87a81139eb88780d9b46ce
buitranduytuan <btdt94@gmail.com>
2020-09-27 09:29:54 +0700

    update code project

--- src/tuanbtd/com/Person.java
+++ src/tuanbtd/com/Person.java
@@ -2,4 +2,24 @@ package tuanbtd.com;
 
 public class Person {
 	private String address;
+	
+	private boolean gender;
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public boolean isGender() {
+		return gender;
+	}
+
+	public void setGender(boolean gender) {
+		this.gender = gender;
+	}
+	
+	
 }
--- src/tuanbtd/com/Student.java
+++ src/tuanbtd/com/Student.java
@@ -1,6 +1,6 @@
 package tuanbtd.com;
 
-public class Student {
+public class Student extends Person {
 	private String name;
 	private int age;
 
