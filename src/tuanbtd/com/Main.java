815d1c36a7a6dbe13be093ac1dde289ded4e160c
buitranduytuan <btdt94@gmail.com>
2020-09-27 09:50:20 +0700

    edit class Main

--- src/tuanbtd/com/Main.java
+++ src/tuanbtd/com/Main.java
@@ -3,4 +3,7 @@ package tuanbtd.com;
 public class Main {
 
 	Student st = new Student("TuanBTD", 18);
+	Student st2 = new Student("TuanBTD2", 20);
+	// day la code ben branch Develop
+	Student st3 = new Student("TuanBTD2", 20);
 }
